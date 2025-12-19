87d1f312b40bc218e21132d774f2ae7afd23c2bd
jose <jduvmx@gmail.com>
2025-12-19 16:04:59 -0600

    Enhance theme toggle button in navbar for improved accessibility and user experience

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -20,10 +20,14 @@
     <flux:spacer />
 
     <div class="flex items-center gap-2">
-        <flux:dropdown align="end">
-            <flux:button variant="subtle" icon="moon" class="dark:hidden" aria-label="Dark mode" />
-            <flux:button variant="subtle" icon="sun" class="hidden dark:flex" aria-label="Light mode" />
-            
+        <flux:dropdown x-data align="end">
+            <flux:button variant="subtle" square class="group" aria-label="Preferred color scheme">
+                <flux:icon.sun x-show="$flux.appearance === 'light'" variant="mini" class="text-zinc-500 dark:text-white" />
+                <flux:icon.moon x-show="$flux.appearance === 'dark'" variant="mini" class="text-zinc-500 dark:text-white" />
+                <flux:icon.moon x-show="$flux.appearance === 'system' && $flux.dark" variant="mini" />
+                <flux:icon.sun x-show="$flux.appearance === 'system' && ! $flux.dark" variant="mini" />
+            </flux:button>
+
             <flux:menu>
                 <flux:menu.item icon="sun" x-on:click="$flux.appearance = 'light'">Light</flux:menu.item>
                 <flux:menu.item icon="moon" x-on:click="$flux.appearance = 'dark'">Dark</flux:menu.item>
